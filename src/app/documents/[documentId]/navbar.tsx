19594843194148580dbb790bfdfca3b08868701e
邓攀 <571143755@qq.com>
2025-08-14 16:32:10 +0800

    feat: add remove, rename dialog

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -9,9 +9,11 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { Avatars } from "./avatars"
 import { Inbox } from "./inbox"
 import { Doc } from "../../../../convex/_generated/dataModel"
+import { api } from "../../../../convex/_generated/api"
+import { RenameDialog } from "@/components/rename-dialog"
+import { RemoveDialog } from "@/components/remove-dialog"
 import { useEditorStore } from "@/store/use-editor-store"
 import { useMutation } from "convex/react"
-import { api } from "../../../../convex/_generated/api"
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 
@@ -116,14 +118,29 @@ export const Navbar = ({ data }: NavbarProps) => {
                     New Document
                   </MenubarItem>
                   <MenubarSeparator />
-                  <MenubarItem>
-                    <FilePenIcon className="size-4 mr-2"/>
-                    Rename
-                  </MenubarItem>
-                  <MenubarItem>
-                    <TrashIcon className="size-4 mr-2"/>
-                    Remove
-                  </MenubarItem>
+                  <RenameDialog
+                    documentId={data._id}
+                    initialTitle={data.title}
+                  > 
+                    <MenubarItem 
+                      onClick={(e) => e.stopPropagation()}
+                      onSelect={(e) => e.preventDefault()}
+                    >
+                      <FilePenIcon className="size-4 mr-2"/>
+                      Rename
+                    </MenubarItem>
+                  </RenameDialog>
+                  <RemoveDialog
+                    documentId={data._id}
+                  >
+                    <MenubarItem 
+                      onClick={(e) => e.stopPropagation()}
+                      onSelect={(e) => e.preventDefault()}
+                    >
+                      <TrashIcon className="size-4 mr-2"/>
+                      Remove
+                    </MenubarItem>
+                  </RemoveDialog>
                   <MenubarSeparator />
                   <MenubarItem onClick={() => window.print()}>
                     <PrinterIcon className="size-4 mr-2"/>
